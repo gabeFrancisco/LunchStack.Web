0886f3429f1ed5e3c5fec7657491b5a02a192ebf
gabeFrancisco <gabegit2@gmail.com>
2024-08-21 15:07:26 -0300

    added formik and started form on login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,50 @@
+import { useFormik } from "formik";
+
 function LoginPage() {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    enableReinitialize: true,
+    validateOnChange: false,
+    onSubmit: () => {},
+  });
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen">
       <div className="p-3 -mt-24 border border-gray-200 rounded shadow-sm">
-        Login
+        <h1>Login</h1>
+        <form className="flex flex-col">
+          <div className="my-2">
+            <input
+              className="border border-gray-200 p-0.5"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Usuário"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div className="my-2">
+            <input
+              className="border border-gray-200 p-0.5"
+              type="text"
+              id="password"
+              name="password"
+              placeholder="Senha"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+            />
+          </div>
+          <button
+            type="button"
+            className="px-3 py-1 text-white rounded bg-primary hover:bg-blue-900"
+          >
+            Entrar!
+          </button>
+        </form>
       </div>
     </div>
   );
