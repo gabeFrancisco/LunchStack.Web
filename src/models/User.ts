a9b30e9ca9ebfd4df967010da7699fe7fe8a825e
gabeFrancisco <gabegit2@gmail.com>
2024-08-25 15:54:35 -0300

    created BaseEntity and User models

---
+++ src/models/User.ts
@@ -0,0 +1,9 @@
+import { BaseEntity } from "./BaseEntity";
+
+export interface User extends BaseEntity{
+  username: string;
+  name: string;
+  email: string;
+  role: number;
+  lastUserWorkGroup: number;
+}
