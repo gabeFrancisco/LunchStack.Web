6d1f57f7df2e98a70a06f3e4f4853b8ad4620052
gabeFrancisco <gabegit2@gmail.com>
2024-10-07 14:22:35 -0300

    react modal first implementation on categories

--- src/components/Category/CategoryToolbar.tsx
+++ src/components/Category/CategoryToolbar.tsx
@@ -1,7 +1,11 @@
-function CategoryToolbar() {
+interface Props{
+  newCategoryModalState: () => void
+}
+
+function CategoryToolbar(props: Props) {
   return (
     <div className="flex flex-row w-full p-2 my-3 border-b">
-      <button className="px-3 py-1 text-sm text-white rounded bg-primary hover:bg-blue-900">
+      <button onClick={props.newCategoryModalState} className="px-3 py-1 text-sm text-white rounded bg-primary hover:bg-blue-900">
         Nova Categoria!
       </button>
     </div>
--- src/pages/dashboard/categorias.tsx
+++ src/pages/dashboard/categorias.tsx
@@ -1,16 +1,29 @@
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import CategoryTable from "../../components/Category/CategoryTable";
 import CategoryToolbar from "../../components/Category/CategoryToolbar";
+import { useEffect, useState } from "react";
+import Modal from 'react-modal'
 
 function CategoriasPage() {
+  const [newCategoryModal, setNewCategoryModal] = useState(false);
+  const handleNewCategoryModal = () =>
+    newCategoryModal ? setNewCategoryModal(false) : setNewCategoryModal(true);
+
+  useEffect(() => {
+    console.log(newCategoryModal)
+  }, [newCategoryModal])
   return (
     <div>
       <SectionTitle
         title="Categorias"
         subtitle="Gerencie todas as suas categorias nessa seção!"
       />
-      <CategoryToolbar/>
+      <CategoryToolbar newCategoryModalState={handleNewCategoryModal} />
       <CategoryTable />
+      <Modal isOpen={newCategoryModal} shouldCloseOnOverlayClick={true}>
+        Nova categoria!
+        <button onClick={handleNewCategoryModal}>Fechar</button>
+      </Modal>
     </div>
   );
 }
