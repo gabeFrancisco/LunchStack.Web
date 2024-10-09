23ff27a3e04eca18f9e143dccb6c497416136d17
gabeFrancisco <gabegit2@gmail.com>
2024-10-08 22:52:52 -0300

    basic ProductCard styling and info show

--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -1,6 +1,15 @@
-function ProductCard() {
-  return ( <div className="w-full m-2 rounded shadow">
-    
+interface Props{
+  name: string;
+  categoryId: number,
+  quantity: number,
+  price: number;
+}
+
+function ProductCard(props: Props) {
+  return ( <div className="flex flex-row w-full p-2 my-2 rounded shadow">
+    <p className="mx-2">{props.name}</p>
+    <p className="mx-2">{props.quantity}</p>
+    <p className="mx-2">{props.price.toFixed(2)}</p>
   </div> );
 }
 
--- src/components/Products/ProductList.tsx
+++ src/components/Products/ProductList.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/store";
 import { getAllProductsAsync } from "../../store/slices/productSlice";
 import Skeleton from "react-loading-skeleton";
+import ProductCard from "./ProductCard";
 
 function ProductList() {
   const products = useAppSelector(state => state.products.productList);
@@ -12,7 +13,7 @@ function ProductList() {
   return <div className="flex flex-col w-full">
     {products.length > 0 ? (
       products.map((el, index) => (
-        <p>{el.name}</p>
+        <ProductCard key={index} categoryId={0} name={el.name} quantity={el.quantity} price={el.price}/>
       ))
     ) : (
       <Skeleton count={3}/>
