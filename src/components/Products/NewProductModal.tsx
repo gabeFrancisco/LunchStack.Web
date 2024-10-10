8c883b648277720fe6ca03d2c1340c3bd051a739
gabeFrancisco <gabegit2@gmail.com>
2024-10-10 14:37:26 -0300

    started NewProductModal form

--- src/components/Products/NewProductModal.tsx
+++ src/components/Products/NewProductModal.tsx
@@ -9,12 +9,61 @@ interface Props {
 function NewProductModal(props: Props) {
   const dispatch = useAppDispatch();
   const formik = useFormik({
-    initialValues: {},
+    initialValues: {
+      name: "",
+      categoryId: 0,
+      quantity: 0,
+      price: 0,
+    },
     onSubmit: () => {},
   });
   return (
     <Modal toggleModal={props.handleClose} title="Novo produto">
-      <form onSubmit={formik.handleSubmit}></form>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="flex flex-row w-full m-2">
+          <label htmlFor="name">Nome do produto: </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Nome"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            className="border"
+          />
+        </div>
+        <div className="flex flex-row w-full m-2">
+        <label htmlFor="quantity">Quantidade: </label>
+          <input
+            type="text"
+            name="quantity"
+            id="quantity"
+            placeholder="Nome"
+            value={formik.values.quantity}
+            onChange={formik.handleChange}
+            className="border"
+          />
+        </div>
+        <div className="flex flex-row w-full m-2">
+        <label htmlFor="category">Categoria:</label>
+          <select>
+            <option>Burgers</option>
+          </select>
+        </div>
+        <div className="flex flex-row w-full m-2">
+          <label htmlFor="price">Preço: </label>
+          <input
+            type="text"
+            name="price"
+            id="price"
+            placeholder="Nome"
+            value={formik.values.price}
+            onChange={formik.handleChange}
+            className="border"
+          />
+        </div>
+        <button onClick={props.handleClose}>Fechar</button>
+      </form>
     </Modal>
   );
 }
--- src/pages/dashboard/produtos.tsx
+++ src/pages/dashboard/produtos.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ProductList from "../../components/Products/ProductList";
 import ProductToolbar from "../../components/Products/ProductToolbar";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import NewProductModal from "../../components/Products/NewProductModal";
 
 function ProdutosPage() {
   const [newProductModal, setNewProductModal] = useState(false);
@@ -10,8 +11,8 @@ function ProdutosPage() {
   return ( <div>
     <SectionTitle title="Produtos" subtitle="Gerencie todos os seus produtos por aqui."/>
     <ProductToolbar newProductModalState={handleNewProductModal}/>
-
     <ProductList/>
+    {newProductModal && <NewProductModal handleClose={handleNewProductModal}/>}
   </div> );
 }
 
