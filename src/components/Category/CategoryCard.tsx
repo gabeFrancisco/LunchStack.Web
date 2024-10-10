a3ecf238e9456ca38bb7ae3babeba8fc1da18e73
gabeFrancisco <gabegit2@gmail.com>
2024-10-10 14:22:34 -0300

    fixed CategoryCard click bug

--- src/components/Category/CategoryCard.tsx
+++ src/components/Category/CategoryCard.tsx
@@ -14,21 +14,22 @@ function CategoryCard(props: Props) {
   const handleInfoModal = () =>
     infoModal ? setInfoModal(false) : setInfoModal(true);
   return (
-    <div
-      onClick={handleInfoModal}
-      className="relative flex flex-row justify-between w-full m-1 rounded shadow cursor-pointer hover:bg-slate-200"
-      key={props.index}
-    >
-      <div className="mr-5">
-        <div
-          style={{ backgroundColor: props.color }}
-          className="h-full p-8 text-center rounded-tl rounded-bl "
-        ></div>
-      </div>
-      <div className="px-4 py-4 text-xl font-bold truncate">{props.name}</div>
-      <div className="flex flex-row items-center px-4 py-4 text-lg font-bold border-l">
-        <BiFoodMenu className="mr-1" />
-        {props.quantity}
+    <div key={props.index}>
+      <div
+        className="relative flex flex-row justify-between w-full m-1 rounded shadow cursor-pointer hover:bg-slate-200"
+        onClick={handleInfoModal}
+      >
+        <div className="mr-5">
+          <div
+            style={{ backgroundColor: props.color }}
+            className="h-full p-8 text-center rounded-tl rounded-bl "
+          ></div>
+        </div>
+        <div className="px-4 py-4 text-xl font-bold truncate">{props.name}</div>
+        <div className="flex flex-row items-center px-4 py-4 text-lg font-bold border-l">
+          <BiFoodMenu className="mr-1" />
+          {props.quantity}
+        </div>
       </div>
       {infoModal && (
         <CategoryInfoModal
