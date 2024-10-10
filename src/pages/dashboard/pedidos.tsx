391e96d2bf695c049aedb6fdbce4ccff9962bc53
gabeFrancisco <gabegit2@gmail.com>
2024-10-10 14:16:24 -0300

    newProductModal logic

--- src/pages/dashboard/pedidos.tsx
+++ src/pages/dashboard/pedidos.tsx
@@ -1,9 +1,14 @@
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
 function PedidosPage() {
-  return <div>
-    <SectionTitle title="Pedidos" subtitle="Gerencie seus pedidos nesta seção."/>
-  </div>;
+  return (
+    <div>
+      <SectionTitle
+        title="Pedidos"
+        subtitle="Gerencie seus pedidos nesta seção."
+      />
+    </div>
+  );
 }
 
 export default PedidosPage;
--- src/pages/dashboard/produtos.tsx
+++ src/pages/dashboard/produtos.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import ProductList from "../../components/Products/ProductList";
+import ProductToolbar from "../../components/Products/ProductToolbar";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
 function ProdutosPage() {
+  const [newProductModal, setNewProductModal] = useState(false);
+  const handleNewProductModal = () =>
+    newProductModal ? setNewProductModal(false) : setNewProductModal(true);
   return ( <div>
     <SectionTitle title="Produtos" subtitle="Gerencie todos os seus produtos por aqui."/>
+    <ProductToolbar newProductModalState={handleNewProductModal}/>
+
     <ProductList/>
   </div> );
 }
